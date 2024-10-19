4cf212ce0dfe65f8825dbdfff389747040809b3d
Michael Neal <mjn@vt.edu>
2024-10-18 23:30:12 -0400

    Fixed background images

--- travelbuddy/src/Login.js
+++ travelbuddy/src/Login.js
@@ -34,24 +34,27 @@ export default function Login() {
 
 
     return (
-        <div class="login-container">
-            <form class="login-form">
-                <div class="form-header-group">
-                    <img class="bonvoy-img" src="bonvoy-logo.png" alt="Mariott Bonvoy Logo" width="50%"></img>
-                    <h1 class="testing">Login</h1>
-                </div>
-                <div class="input-group">
-                    <label for="username">Username</label>
-                    <input type="text" id="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                </div>
-                <div class="input-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-                <button type="button" onClick={handleLogin}>Login</button>
-                <p>Need an account? <Link to="/">Sign Up</Link></p>
-            </form>
+        <div class="login-page-container">
+            <div class="login-container">
+                <form class="login-form">
+                    <div class="form-header-group">
+                        <img class="bonvoy-img" src="bonvoy-logo.png" alt="Mariott Bonvoy Logo" width="50%"></img>
+                        <h1 class="testing">Login</h1>
+                    </div>
+                    <div class="input-group">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div class="input-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <button type="button" onClick={handleLogin}>Login</button>
+                    <p>Need an account? <Link to="/">Sign Up</Link></p>
+                </form>
+            </div>
         </div>
+        
   );
 }
 
--- travelbuddy/src/SignUp.js
+++ travelbuddy/src/SignUp.js
@@ -32,23 +32,25 @@ export default function SignUp() {
     };
 
     return (
-    <div class="login-container">
-        <form class="login-form">
-            <div class="form-header-group">
-                <img class="bonvoy-img" src="bonvoy-logo.png" alt="Mariott Bonvoy Logo" width="50%"></img>
-                <h1 class="testing">Create an Account</h1>
-            </div>
-            <div class="input-group">
-                <label for="username">Username</label>
-                <input type="text" id="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-            </div>
-            <div class="input-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            <button type="button" onClick={handleSignUp}>Sign Up</button>
-            <p>Already have an account? <Link to="/login">Login</Link></p>
-        </form>
+    <div class="signup-page-container">
+        <div class="login-container">
+            <form class="login-form">
+                <div class="form-header-group">
+                    <img class="bonvoy-img" src="bonvoy-logo.png" alt="Mariott Bonvoy Logo" width="50%"></img>
+                    <h1 class="testing">Create an Account</h1>
+                </div>
+                <div class="input-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div class="input-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button type="button" onClick={handleSignUp}>Sign Up</button>
+                <p>Already have an account? <Link to="/login">Login</Link></p>
+            </form>
+        </div>
     </div>
   );
 }
