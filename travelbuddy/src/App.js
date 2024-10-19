0a0358843b20d58416b31c1c614b9a44e2bfb134
Michael Neal <mjn@vt.edu>
2024-10-18 20:36:44 -0400

    Completed sign up form

--- travelbuddy/src/App.js
+++ travelbuddy/src/App.js
@@ -1,12 +1,14 @@
 import SignUp from './SignUp';
 import Login from './Login';
 import './App.css';
+import './SignUp.css'
+import './Login.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    
+
     <Router>
       <Routes>
           <Route path="/" element={<SignUp />} />
--- travelbuddy/src/SignUp.js
+++ travelbuddy/src/SignUp.js
@@ -1,6 +1,42 @@
 import './SignUp.css';
 import { Link }  from 'react-router-dom'
 
+function send_signup_credentials() {
+    const ipAddress = '172.31.104.7';
+    const port = '5000';
+    const url = `http://${ipAddress}:${port}`;
+
+    const fetchRequest = fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: 'myTestUsername', pass:'myTestPassword'})
+    });
+
+    const timeout = new Promise((_, reject) =>
+        setTimeout(() => reject(new Error('Request timed out')), 2000)
+    );
+
+
+    Promise.race([fetchRequest, timeout])
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+            alert(`Response: ${data.message}`);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('An error occurred while sending the request.');
+        });
+    
+}
+
 export default function SignUp() {
   return (
     <div class="login-container">
@@ -17,7 +53,7 @@ export default function SignUp() {
                 <label for="password">Password</label>
                 <input type="password" id="password" placeholder="Enter your password"/>
             </div>
-            <button type="button" onclick="login()">Sign Up</button>
+            <button type="button" onClick={send_signup_credentials}>Sign Up</button>
             <p>Already have an account? <Link to="/login">Login</Link></p>
         </form>
     </div>
