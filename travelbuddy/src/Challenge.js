614e726b5ee617821b051c8cffdc02409f0712ae
timbuk21221 <tannertownsend10@gmail.com>
2024-10-19 11:42:10 -0400

    Added Challenge Cards

--- travelbuddy/src/Challenge.js
+++ travelbuddy/src/Challenge.js
@@ -1,33 +1,22 @@
-import './Challenge.css'
+import React from 'react';
+import './Challenge.css';
 
-export default function Challenge( {challengeInfo} ) {
+const ChallengeCard = ({ questName, description, reward }) => {
+  return (
+    <div className="challenge-card">
+      <div className="card-header">
+        <h2 className="quest-name">{questName}</h2>
+      </div>
+      <div className="card-body">
+        <p className="description">{description}</p>
+      </div>
+      <div className="reward-section">
+        <h3>Reward:</h3>
+        <p className="reward">{reward}</p>
+      </div>
+    </div>
+  );
+};
 
-    const challengeName = challengeInfo[0];
-    const challengeReward = challengeInfo[1];
-    const challengeDesc = challengeInfo[2];
+export default ChallengeCard;
 
-    return(
-        <div class="challenge-container">
-            <div class="challenge-name-container">
-                <h1> {challengeName} </h1>
-                <div class="challenge-name-line"></div>
-            </div>
-
-            <div class="challenge-description-container">
-                <h1> Description </h1>
-                <div class="challenge-name-line"></div>
-                <p> {challengeDesc} </p>
-            </div>
-
-            <div class="challenge-reward-container">
-                <h1> Reward </h1>
-                <div class="challenge-reward-line"></div>
-                <p> {challengeReward} </p>
-            </div>
-
-            
-        </div>
-    );
-
-
-}
