37c1ee6c65694f8cdf1184bff69b1fafa4c509b1
pl450b <pl450b@outlook.com>
2024-10-20 14:15:53 +0000

    update user prefences

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from database_interaction import attempt_login, add_user, add_token, get_user_from_token, update_selected_challenge
+from database_interaction import attempt_login, add_user, add_token, get_user_from_token, update_selected_challenge, update_user_preferences
 from ai import *
 # Load environment variables from .env file
 load_dotenv()
