4df7b68a70162577416d998463d5d83caba260b8
pl450b <pl450b@outlook.com>
2024-10-20 16:01:44 +0000

    changed url

--- backend/main.py
+++ backend/main.py
@@ -55,8 +55,7 @@ def login():
         token = generate_jwt(user_id)
         add_token(username, token)        
         # Redirect the user to the /personalize route
-        frontend_url = f"{request.host_url}personalize"
-        return redirect(frontend_url)
+        return redirect('http://172.31.104.7:5000/personalize')
     else:
         print(f"failed login from {username}")
         # Return a 401 Unauthorized response if the credentials are invalid
